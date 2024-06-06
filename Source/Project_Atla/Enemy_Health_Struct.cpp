f8192c05f35b760b105ffb43c7f8d17edf3befc5
libanali <43754122+libanali@users.noreply.github.com>
2024-06-06 04:47:53 +0100

    - Made some changes to enemy health system

---
+++ Source/Project_Atla/Enemy_Health_Struct.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enemy_Health_Struct.h"
+
+Enemy_Health_Struct::Enemy_Health_Struct()
+{
+}
+
+Enemy_Health_Struct::~Enemy_Health_Struct()
+{
+}
---
+++ Source/Project_Atla/Enemy_Health_Struct.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class PROJECT_ATLA_API Enemy_Health_Struct
+{
+public:
+	Enemy_Health_Struct();
+	~Enemy_Health_Struct();
+};
