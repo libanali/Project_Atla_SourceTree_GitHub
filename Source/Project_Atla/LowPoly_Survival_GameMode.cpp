004e15900b46cb6547f0214670630e38540be2f5
libanali <43754122+libanali@users.noreply.github.com>
2024-06-07 04:40:26 +0100

    - Created a new character class and game mode class for top down survival game

---
+++ Source/Project_Atla/LowPoly_Survival_GameMode.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LowPoly_Survival_GameMode.h"
+
---
+++ Source/Project_Atla/LowPoly_Survival_GameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "LowPoly_Survival_GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_ATLA_API ALowPoly_Survival_GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
