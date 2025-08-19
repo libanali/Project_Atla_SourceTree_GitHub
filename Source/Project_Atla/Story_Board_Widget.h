015712d058b3d968896ae37f9496837c41845d2d
libanali <43754122+libanali@users.noreply.github.com>
2025-08-19 05:04:34 +0100

    added new story board widget class

---
+++ Source/Project_Atla/Story_Board_Widget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Story_Board_Widget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_ATLA_API UStory_Board_Widget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
